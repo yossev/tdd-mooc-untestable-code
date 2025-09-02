65163bc0b40c11b2bc65912442e9c05ac06bf681
Youssef Hesham <synthpyt@gmail.com>
2025-09-02 19:39:22 +0300

    Untestable2 now works

--- src/untestable2.mjs
+++ src/untestable2.mjs
@@ -1,12 +1,10 @@
-function diceRoll() {
+export function diceRoll() {
   const min = 1;
   const max = 6;
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
-export function diceHandValue() {
-  const die1 = diceRoll();
-  const die2 = diceRoll();
+export function diceHandValue(die1, die2) {
   if (die1 === die2) {
     // one pair
     return 100 + die1;
--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -1,10 +1,24 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
 import { diceHandValue } from "../src/untestable2.mjs";
+import { diceRoll } from "../src/untestable2.mjs";
 
-describe("Untestable 2: a dice game", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(diceHandValue()).to.be.a("number");
+describe("Randomness: A Dice game", () => {
+  test("dice rolls are between 1 - 6", () => {
+    const rolls = new Set();
+    for(let i = 0; i < 100; i++){
+      rolls.add(diceRoll());
+    }
+    expect(rolls).to.have.all.keys([1, 2, 3, 4, 5, 6]);
   });
+
+  test("one pair", () => {
+    expect(diceHandValue(1, 1)).to.equal(101);
+    expect(diceHandValue(6, 6)).to.equal(106);
+  })
+
+  test("high die", () => {
+    expect(diceHandValue(1, 2)).to.equal(2);
+    expect(diceHandValue(4, 6)).to.equal(6);
+  })
 });
