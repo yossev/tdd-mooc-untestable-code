1aebb28a1bbe9638e825c4514da1fb4f72724807
Youssef Hesham <synthpyt@gmail.com>
2025-09-12 03:58:29 +0300

    Strategy Design Pattern

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -59,7 +59,8 @@ describe("Globals and singletons: enterprise application", () => {
     expect(hasher.verifyPassword(userAfter.passwordHash, "old-pw")).to.be.true;
   }
 
-  // Contract testing, testing both the fake and the real password hasers
+  // Contract testing, testing both the fake and the real password hashers
+  // during contract testing a function is created to be fed with 
   function PasswordHasherContract(hasher) {
     const hash = hasher.hashPassword("correct");
 
@@ -91,4 +92,51 @@ describe("Globals and singletons: enterprise application", () => {
   
   });
   })
+
+
+  function UserDaoContract(daoProvider) {
+  let userIdSeq = 100;
+  let dao;
+  beforeEach(() => {
+    dao = daoProvider();
+  });
+
+  test("get user by ID", async () => {
+    const user1a = {
+      userId: ++userIdSeq,
+      passwordHash: "abc",
+    };
+    const user2a = {
+      userId: ++userIdSeq,
+      passwordHash: "xyz",
+    };
+    await dao.save(user1a);
+    await dao.save(user2a);
+
+    const user1b = await dao.getById(user1a.userId);
+    const user2b = await dao.getById(user2a.userId);
+    expect(user1b).to.deep.equal(user1a, "get user1 by ID");
+    expect(user2b).to.deep.equal(user2a, "get user2 by ID");
+    expect(user1b).to.not.equal(user1a, "makes defensive copies");
+    expect(await dao.getById(666)).to.equal(null, "non-existing ID");
+  });
+
+  test("create and update user", async () => {
+    const user = {
+      userId: ++userIdSeq,
+      passwordHash: "abc"
+    };
+    await dao.save(user);
+    expect(await dao.getById(user.userId)).to.deep.equal(user, "after create");
+
+    user.passwordHash("xyz");
+
+
+    expect(await dao.getById(user.userId)).to.not.deep.equal(user, "before update");
+    await dao.save(user);
+    expect(await dao.getById(user.userId)).to.deep.equal(user, "after update");
+  })
+  }
+
+
 });
